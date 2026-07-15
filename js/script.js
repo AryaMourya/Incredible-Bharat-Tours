787fe87a282c9c2ac9e42a9cd80be139b6ef1bef
AryaMourya <aryamourya0@gmail.com>
2026-07-15 19:14:30 +0530

    commit and save all changes

--- js/script.js
+++ js/script.js
@@ -19,3 +19,32 @@ searchInput.addEventListener("keyup",function(){
         }
     });
 });
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("keyup", () => {
+    const value = searchInput.value.toLowerCase();
+
+    document.querySelectorAll(".card").forEach(card => {
+        const text = card.innerText.toLowerCase();
+
+        card.style.display = text.includes(value) ? "block" : "none";
+    });
+});
+
+// Dark Mode 
+
+const themeToggle = document.getElementById('theme-toggle');
+
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark-mode");
+}
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.classList.contains('dark-mode')){
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        localStorage.setItem("theme", "light");
+    }
+});
